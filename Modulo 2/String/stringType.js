0119026e5fbb64b648e4a5eb3ce77c66ddbe7b2a
andreymafra55 <andreymafra55@gmail.com>
2022-11-29 14:50:12 -0300

    Estudo do tipo string

---
+++ Modulo 2/String/stringType.js
@@ -0,0 +1,11 @@
+"use strict";
+//===> Exemplo 01 -Single Quotes
+let nomeCompleto = "Andrey Mafra";
+console.log(nomeCompleto);
+//===> Exemplo 02 -Double Quotes
+let funcaoEmpresa = "Estagiario de TypeScript";
+console.log(funcaoEmpresa);
+//===> Exemplo 03 -Back Ticks
+let nomeEmpresa = 'Microsoft';
+let dadosFuncionario = `Seja Bem-vindo ${nomeCompleto}! Você será ${funcaoEmpresa}`;
+console.log(dadosFuncionario);
---
+++ Modulo 2/String/stringType.ts
@@ -0,0 +1,17 @@
+//===> Exemplo 01 -Single Quotes
+
+let nomeCompleto: string = "Andrey Mafra";
+console.log(nomeCompleto);
+
+//===> Exemplo 02 -Double Quotes
+
+let funcaoEmpresa: string = "Estagiario de TypeScript";
+console.log(funcaoEmpresa);
+
+//===> Exemplo 03 -Back Ticks
+
+let nomeEmpresa: string = 'Microsoft';
+ 
+let dadosFuncionario: string =  `Seja Bem-vindo ${nomeCompleto}! Você será ${funcaoEmpresa}`;
+
+console.log(dadosFuncionario);
