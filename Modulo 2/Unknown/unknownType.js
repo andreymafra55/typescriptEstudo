25150aed6419f1950298b1d59003ac92ec20a3c2
andreymafra55 <andreymafra55@gmail.com>
2022-11-29 21:52:59 -0300

    feat: estudo unknown

---
+++ Modulo 2/Unknown/unknownType.js
@@ -0,0 +1,18 @@
+"use strict";
+//===> Exemplo 1: Tipo Unknown
+let valorVariavel;
+valorVariavel = true;
+valorVariavel = 123;
+valorVariavel = [];
+valorVariavel = 'Andrey';
+//===> Exemplo 2: Erro ao tentar atribuir uma valor do tipo 'unknown' a outros tipos
+/*
+let valor:unknown;
+
+let valor1:boolean = valor;
+*/
+//===> Exemplo 3: Diferença entre: 'any' vs 'unknown'
+let algumaCoisaAny;
+let algumaCoisaUnknown;
+console.log(algumaCoisaAny.toFixed(2));
+console.log(algumaCoisaUnknown.toFixed(2));
---
+++ Modulo 2/Unknown/unknownType.ts
@@ -0,0 +1,23 @@
+//===> Exemplo 1: Tipo Unknown
+
+let valorVariavel:unknown;
+
+valorVariavel = true;
+valorVariavel = 123;
+valorVariavel = [];
+valorVariavel = 'Andrey';
+
+//===> Exemplo 2: Erro ao tentar atribuir uma valor do tipo 'unknown' a outros tipos
+/*
+let valor:unknown;
+
+let valor1:boolean = valor;
+*/
+
+//===> Exemplo 3: Diferença entre: 'any' vs 'unknown'
+
+let algumaCoisaAny: any;
+let algumaCoisaUnknown: unknown;
+
+console.log(algumaCoisaAny.toFixed(2));
+console.log(algumaCoisaUnknown.toFixed(2));
