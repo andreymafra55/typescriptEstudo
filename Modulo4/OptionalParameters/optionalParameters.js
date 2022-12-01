35b7f181627b558673b6d83440de797cd66a1a92
andreymafra55 <andreymafra55@gmail.com>
2022-12-01 15:55:58 -0300

    feat: estudo de optional parameters

--- Modulo4/OptionalParameters/optionalParameters.js
+++ Modulo4/OptionalParameters/optionalParameters.js
@@ -1,4 +1,23 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 // ==> Exemplo 01 - Optional Parameter
+function informarDadosPessoa(idPessoa, nome, email) {
+    console.log('Id Funcionario... : ', idPessoa, '\nNome do funcionario... : ', nome);
+    if (email != undefined)
+        console.log('Email... : ', email);
+}
+informarDadosPessoa(1, 'Andrey', 'Andrey@email.com');
+informarDadosPessoa(2, 'heitor');
 // ==> Exemplo 02
-// ==> Exemplo 03
+function mensagemLog(mensagem, usuarioId) {
+    const horaLog = new Date().toLocaleTimeString();
+    console.log(horaLog, mensagem, usuarioId || 'Usuario não conectado');
+}
+mensagemLog('Atualizar Pagina.');
+mensagemLog('Usuario logado com sucesso', 1);
+let pessoa;
+pessoa = {
+    IdFuncionario: 55,
+    nome: 'Andrey',
+};
+console.log(pessoa);
--- Modulo4/OptionalParameters/optionalParameters.ts
+++ Modulo4/OptionalParameters/optionalParameters.ts
@@ -1,3 +1,40 @@
+export{}
 // ==> Exemplo 01 - Optional Parameter
+
+function informarDadosPessoa(idPessoa:number,nome:string,email?:string){
+    console.log('Id Funcionario... : ',idPessoa,'\nNome do funcionario... : ',nome);
+
+    if (email != undefined) console.log('Email... : ',email);
+} 
+
+informarDadosPessoa(1,'Andrey','Andrey@email.com');
+informarDadosPessoa(2,'heitor');
+
 // ==> Exemplo 02
+
+function mensagemLog(mensagem: string,usuarioId?:number) {
+    const horaLog = new Date().toLocaleTimeString();
+
+    console.log(horaLog,mensagem,usuarioId || 'Usuario não conectado');
+}
+mensagemLog('Atualizar Pagina.');
+mensagemLog('Usuario logado com sucesso',1);
+
+
 // ==> Exemplo 03
+
+type Pessoa = {
+    IdFuncionario:number;
+    nome:string;
+    idade?:number;
+    email?:string;
+}
+
+let pessoa:Pessoa;
+
+pessoa = {
+    IdFuncionario: 55,
+    nome: 'Andrey',
+}
+
+console.log(pessoa);
