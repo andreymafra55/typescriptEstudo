6e8fdbc7a9fb204a2f1e7f606df44dd70f6735c0
andreymafra55 <andreymafra55@gmail.com>
2022-11-30 22:52:50 -0300

    feat: estudo dos tipos null e undefined

---
+++ Modulo 2/Null&Undefined/null&Undefined.js
@@ -0,0 +1,12 @@
+"use strict";
+// ==> Exemplo 01: null 
+let variavelNull = null;
+console.log(variavelNull);
+console.log(typeof (variavelNull));
+// ==> Exemplo 02: Undefined
+let variavelUndefined;
+console.log(variavelUndefined);
+console.log(typeof (variavelUndefined));
+// Null x Undefined
+console.log('Exemplo 1:', null == undefined);
+console.log('Exemplo 2:', null === undefined);
